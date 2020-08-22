de3d4e443e83a3297c879c5496eb3793a9765f4f
Kawish Behzad <kawisphoenix@gmail.com>
2020-08-22 20:57:55 +0700

    Added new docs
    for gh-pages branch replacement

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,122 +1,123 @@
 <template>
-    <div id="app">
-        <div class="wrapper">
-            <section class="vlider-first">
-                <div class="site-container">
-                    <div class="header">
-                        <h1><span class="green">V</span>lider</h1>
-                        <h3>Input range component based on vue and custom CSS3. Highly customizable, pretty, and easy to use.</h3>
-                    </div>
-                    <h2>Install Plugin</h2>
-                    <div class="code-snippet">
-                        <div class="circles"></div>
-                        <pre v-highlightjs><code class="js">npm install --save vlider</code></pre>
-                    </div>
-                    <div class="code-snippet">
-                        <div class="circles"></div>
-                        <pre v-highlightjs><code class="html" v-html="firstExample"></code></pre>
-                    </div>
-                    <h2>Component Usage Example</h2>
-                    <p>Use slot-scope to reiterate the data that you pass to vlider-data.</p>
-                    <div class="code-snippet">
-                        <div class="circles"></div>
-                        <pre v-highlightjs><code class="html" v-html="secondExample"></code></pre>
-                    </div>
-                    <div class="components">
-                        <div class="component full light">
-                            <h4>Light Theme</h4>
-                            <vlider
-                            :id="'light'"
-                            :vlider-data="lightSlider"
-                            :theme="'theme-light'"
-                            v-model="inputRange"
-                            @click="vliderClick"
-                            >
-                                <template slot="bullet" slot-scope="bullet">
-                                    <label>{{ bullet.data.label }}</label>
-                                    <i
-                                    class="em"
-                                    :class="[`em-${bullet.data.extras.icon}`]"
-                                    ></i> 
-                                    <a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
-                                </template>
-                            </vlider>
-                        </div>
-                        <div class="component full dark">
-                            <h4>Dark Theme</h4>
-                            <vlider
-                            :id="'dark'"
-                            :vlider-data="darkSlider"
-                            :theme="'theme-dark'"
-                            v-model="inputRange"
-                            @click="vliderClick"
-                            >
-                                <template slot="bullet" slot-scope="bullet">
-                                    <label>{{ bullet.data.label }}</label>
-                                    <i
-                                    class="em"
-                                    :class="[`em-${bullet.data.extras.icon}`]"
-                                    ></i> 
-                                    <a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
-                                </template>
-                            </vlider>
-                        </div>
-                        <div class="component full purple">
-                            <h4>Purple Theme</h4>
-                            <vlider
-                            :id="'purple'"
-                            :vlider-data="purpleSlider"
-                            :theme="'theme-purple'"
-                            v-model="inputRange"
-                            @click="vliderClick"
-                            >
-                                <template slot="bullet" slot-scope="bullet">
-                                    <label>{{ bullet.data.label }}</label>
-                                    <i
-                                    class="em"
-                                    :class="[`em-${bullet.data.extras.icon}`]"
-                                    ></i> 
-                                    <a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
-                                </template>
-                            </vlider>
-                        </div>
-                    </div>
-                    <h2>Params</h2>
-                    <ul class="demo-ul">
-                        <li><b>id</b> => id of the vlider component</li>
-                        <li><b>classes</b> => to add extra classes to the outer vlider container</li>
-                        <li><b>theme</b> => there are 3 default themes : ("theme-dark", "theme-light", "theme-purple")</li>
-                        <li><b>vlider-data</b> => Format : Create an array of objects, and you have to send label, color, and extras in every object, extras can be filled with anything you want and you can use the extras object using slot-scope like the above example</li>
-                    </ul>
-                    <h2>Events</h2>
-                    <ul class="demo-ul">
-                        <li>
-                            <b>v-model</b> => v-model="yourVariable" //same usage like input v-model
-                        </li>
-                        <li>
-                            <b>click</b> => @click="yourMethodHere(value)" //you can take the clicked element's value by using $event or the first parameter of your method
-                        </li>
-                    </ul>
-                </div>
-            </section>
-        </div>
-    </div>
+	<div id="app">
+		<div class="wrapper">
+			<section class="vlider-first">
+				<div class="site-container">
+					<div class="header">
+						<h1>
+							<span class="green">V</span>lider
+						</h1>
+						<h3>Input range component based on vue and custom CSS3. Highly customizable, pretty, and easy to use.</h3>
+					</div>
+					<h2>Install Plugin</h2>
+					<div class="code-snippet">
+						<div class="circles"></div>
+						<pre v-highlightjs><code class="js">npm install --save vlider</code></pre>
+					</div>
+					<div class="code-snippet">
+						<div class="circles"></div>
+						<pre v-highlightjs><code class="html" v-html="firstExample"></code></pre>
+					</div>
+					<h2>Component Usage Example</h2>
+					<p>Use slot-scope to reiterate the data that you pass to vlider-data.</p>
+					<div class="code-snippet">
+						<div class="circles"></div>
+						<pre v-highlightjs><code class="html" v-html="secondExample"></code></pre>
+					</div>
+					<div class="components">
+						<div class="component full light">
+							<h4>Light Theme</h4>
+							<vlider
+								:id="'light'"
+								:vlider-data="lightSlider"
+								:theme="'theme-light'"
+								v-model="inputRange"
+								@click="vliderClick"
+							>
+								<template slot="bullet" slot-scope="bullet">
+									<label>{{ bullet.data.label }}</label>
+									<i class="em" :class="[`em-${bullet.data.extras.icon}`]"></i>
+									<a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
+								</template>
+							</vlider>
+						</div>
+						<div class="component full dark">
+							<h4>Dark Theme</h4>
+							<vlider
+								:id="'dark'"
+								:vlider-data="darkSlider"
+								:theme="'theme-dark'"
+								v-model="inputRange"
+								@click="vliderClick"
+							>
+								<template slot="bullet" slot-scope="bullet">
+									<label>{{ bullet.data.label }}</label>
+									<i class="em" :class="[`em-${bullet.data.extras.icon}`]"></i>
+									<a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
+								</template>
+							</vlider>
+						</div>
+						<div class="component full purple">
+							<h4>Purple Theme</h4>
+							<vlider
+								:id="'purple'"
+								:vlider-data="purpleSlider"
+								:theme="'theme-purple'"
+								v-model="inputRange"
+								@click="vliderClick"
+							>
+								<template slot="bullet" slot-scope="bullet">
+									<label>{{ bullet.data.label }}</label>
+									<i class="em" :class="[`em-${bullet.data.extras.icon}`]"></i>
+									<a target="_blank" :href="bullet.data.extras.learnMore">Learn more ?</a>
+								</template>
+							</vlider>
+						</div>
+					</div>
+					<h2>Params</h2>
+					<ul class="demo-ul">
+						<li>
+							<b>id</b> => id of the vlider component
+						</li>
+						<li>
+							<b>classes</b> => to add extra classes to the outer vlider container
+						</li>
+						<li>
+							<b>theme</b> => there are 3 default themes : ("theme-dark", "theme-light", "theme-purple")
+						</li>
+						<li>
+							<b>vlider-data</b> => Format : Create an array of objects, and you have to send label, color, and extras in every object, extras can be filled with anything you want and you can use the extras object using slot-scope like the above example
+						</li>
+					</ul>
+					<h2>Events</h2>
+					<ul class="demo-ul">
+						<li>
+							<b>v-model</b> => v-model="yourVariable" //same usage like input v-model
+						</li>
+						<li>
+							<b>click</b> => @click="yourMethodHere(value)" //you can take the clicked element's value by using $event or the first parameter of your method
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 import Vlider from "vlider";
-import VueHighlightJS from 'vue-highlightjs';
-Vue.use(VueHighlightJS)
+import VueHighlightJS from "vue-highlightjs";
+Vue.use(VueHighlightJS);
 
 export default {
-    name: "app",
-    components: {
-        Vlider
-    },
-    data() {
-        return {
-            firstExample: `
+	name: "app",
+	components: {
+		Vlider,
+	},
+	data() {
+		return {
+			firstExample: `
 &#x3C;script&#x3E;
     Import Vlider from &#x22;vlider&#x22;
 
@@ -136,8 +137,8 @@ export default {
     
     @import &#x22;vlider/dist/vlider.min.css&#x22;;
 &#x3C;/style&#x3E;
-            ` ,
-            secondExample: `
+            `,
+			secondExample: `
 &#x3C;template&#x3E;
     &#x3C;vlider
     :id=&#x22;&#x27;first&#x27;&#x22;
@@ -194,173 +195,170 @@ export default {
     @import &#x22;vlider/src/sass/vlider.scss&#x22;
 &#x3C;/style&#x3E;
             `,
-            inputRange: null,
-            lightSlider: [
-                {
-                    label: "Angry",
-                    color: "#3a0088",
-                    extras: {
-                        icon: 'angry',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Expressionless",
-                    color: "#ff5733",
-                    extras: {
-                        icon: 'expressionless',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Astonished",
-                    color: "#c70039",
-                    extras: {
-                        icon: 'astonished',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Confounded",
-                    color: "#ff7657",
-                    extras: {
-                        icon: 'confounded',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Okay?",
-                    color: "#ffc60b",
-                    extras: {
-                        icon: 'face_with_raised_eyebrow',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Blush",
-                    color: "#6effbf",
-                    extras: {
-                        icon: 'blush',
-                        learnMore: 'http://localhost/'
-                    }
-                }
-            ],
-            darkSlider: [
-                {
-                    label: "Angry",
-                    color: "#ffc300",
-                    extras: {
-                        icon: 'angry',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Expressionless",
-                    color: "#ffb0fe",
-                    extras: {
-                        icon: 'expressionless',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Astonished",
-                    color: "#ff6bd6",
-                    extras: {
-                        icon: 'astonished',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Confounded",
-                    color: "#ff9d76",
-                    extras: {
-                        icon: 'confounded',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Okay?",
-                    color: "#51eaea",
-                    extras: {
-                        icon: 'face_with_raised_eyebrow',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Blush",
-                    color: "#fb3569",
-                    extras: {
-                        icon: 'blush',
-                        learnMore: 'http://localhost/'
-                    }
-                }
-            ],
-            purpleSlider: [
-                {
-                    label: "Angry",
-                    color: "#ff8000",
-                    extras: {
-                        icon: 'angry',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Expressionless",
-                    color: "#ddf516",
-                    extras: {
-                        icon: 'expressionless',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Astonished",
-                    color: "#ffe98a",
-                    extras: {
-                        icon: 'astonished',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Confounded",
-                    color: "#ffeed0",
-                    extras: {
-                        icon: 'confounded',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Okay?",
-                    color: "#dbff3d",
-                    extras: {
-                        icon: 'face_with_raised_eyebrow',
-                        learnMore: 'http://localhost/'
-                    }
-                },
-                {
-                    label: "Blush",
-                    color: "#7efaff",
-                    extras: {
-                        icon: 'blush',
-                        learnMore: 'http://localhost/'
-                    }
-                }
-            ],
-        };
-    },
-    watch: {
-        inputRange() {
-            console.log(this.inputRange)
-        }
-    },
-    methods: {
-        vliderClick(value) {
-            console.log(`clicked`)
-            console.log(value)
-        }
-    }
+			inputRange: null,
+			lightSlider: [
+				{
+					label: "Angry",
+					color: "#3a0088",
+					extras: {
+						icon: "angry",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Expressionless",
+					color: "#ff5733",
+					extras: {
+						icon: "expressionless",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Astonished",
+					color: "#c70039",
+					extras: {
+						icon: "astonished",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Confounded",
+					color: "#ff7657",
+					extras: {
+						icon: "confounded",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Okay?",
+					color: "#ffc60b",
+					extras: {
+						icon: "face_with_raised_eyebrow",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Blush",
+					color: "#6effbf",
+					extras: {
+						icon: "blush",
+						learnMore: "http://localhost/",
+					},
+				},
+			],
+			darkSlider: [
+				{
+					label: "Angry",
+					color: "#ffc300",
+					extras: {
+						icon: "angry",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Expressionless",
+					color: "#ffb0fe",
+					extras: {
+						icon: "expressionless",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Astonished",
+					color: "#ff6bd6",
+					extras: {
+						icon: "astonished",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Confounded",
+					color: "#ff9d76",
+					extras: {
+						icon: "confounded",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Okay?",
+					color: "#51eaea",
+					extras: {
+						icon: "face_with_raised_eyebrow",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Blush",
+					color: "#fb3569",
+					extras: {
+						icon: "blush",
+						learnMore: "http://localhost/",
+					},
+				},
+			],
+			purpleSlider: [
+				{
+					label: "Angry",
+					color: "#ff8000",
+					extras: {
+						icon: "angry",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Expressionless",
+					color: "#ddf516",
+					extras: {
+						icon: "expressionless",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Astonished",
+					color: "#ffe98a",
+					extras: {
+						icon: "astonished",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Confounded",
+					color: "#ffeed0",
+					extras: {
+						icon: "confounded",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Okay?",
+					color: "#dbff3d",
+					extras: {
+						icon: "face_with_raised_eyebrow",
+						learnMore: "http://localhost/",
+					},
+				},
+				{
+					label: "Blush",
+					color: "#7efaff",
+					extras: {
+						icon: "blush",
+						learnMore: "http://localhost/",
+					},
+				},
+			],
+		};
+	},
+	watch: {
+		inputRange() {
+			console.log(this.inputRange);
+		},
+	},
+	methods: {
+		vliderClick(value) {
+			console.log(`clicked`);
+			console.log(value);
+		},
+	},
 };
 </script>
 
-<style lang="sass">
-    @import "assets/styles/global.scss";
-</style>
--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "assets/styles/global.scss";
+import "./assets/styles/global.scss";
 
 Vue.config.productionTip = false;
 
